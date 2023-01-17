55ae380aceff692dc47aee6104cffa831d0816c5
Sujay <sujaybk2@yahoo.com>
2023-01-17 01:08:49 -0800

    New classes added

--- src/C1Challenges/Sprint4/ReverseDigitDemo.java
+++ src/C1Challenges/Sprint4/ReverseDigitDemo.java
@@ -4,6 +4,7 @@ public class ReverseDigitDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a number.");
+        String input = scanner.nextLine();
 
     }
 }
